52c9776513f129448c5a01ff90a77ad69bf6310f
BurnsD <Dustinburnsactech@gmail.com>
2021-12-26 22:20:43 -0600

    added scripts and variables

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+const cityFormEl = document.querySelector("#city-search_form");
+const cityInputEl = document.querySelector("#city");
+const weatherContainerEl = document.querySelector("#current-weather_container");
+const citySearchInputEl = document.querySelector("#searched-city");
+const forecastTitle = document.querySelector("#forecast");
+const forecastContainerEl = document.querySelector("#fiveday-container");
+const pastSearchButtonEl = document.querySelector("#past-search_buttons");
